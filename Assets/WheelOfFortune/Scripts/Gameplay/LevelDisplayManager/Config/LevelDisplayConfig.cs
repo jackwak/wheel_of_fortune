edb5773fcd6334787b46236535880de72e4b2623
jackwak <mehmetyanik53@hotmail.com>
2026-05-15 16:46:14 +0300

    Update LevelDisplayConfig.cs

--- Assets/WheelOfFortune/Scripts/Gameplay/LevelDisplayManager/Config/LevelDisplayConfig.cs
+++ Assets/WheelOfFortune/Scripts/Gameplay/LevelDisplayManager/Config/LevelDisplayConfig.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 namespace WheelOfFortune.Gameplay.LevelDisplayManager
 {
-    [CreateAssetMenu(fileName = "LevelDisplayConfig", menuName = "ScriptableObjects/LevelDisplayManager/LevelDisplayConfig")]
+    [CreateAssetMenu(fileName = "LevelDisplayConfig", menuName = "WheelOfFortune/LevelDisplayManager/LevelDisplayConfig")]
     public class LevelDisplayConfig : ScriptableObject
     {
         [SerializeField] private int _maxLevel;
+        [SerializeField] private int _startLevel;
 
         public int MaxLevel => _maxLevel;
+        public int StartLevel => _startLevel;
     }
 }
