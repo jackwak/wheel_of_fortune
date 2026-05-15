23b13062d173ad6ad0c7a77baebf294cbc5806f7
jackwak <mehmetyanik53@hotmail.com>
2026-05-15 17:10:53 +0300

    Update SceneInstaller.cs

--- Assets/WheelOfFortune/Scripts/Core/Zenject/SceneInstaller.cs
+++ Assets/WheelOfFortune/Scripts/Core/Zenject/SceneInstaller.cs
@@ -1,12 +1,19 @@
 using WheelOfFortune.Core.EventBus;
-using WheelOfFortune.Utils;
+using WheelOfFortune.Utils.UIMover;
+using WheelOfFortune.Utils.RankDeterminer;
 using Zenject;
+using UnityEngine;
+using WheelOfFortune.Config;
 
 public class SceneInstaller : MonoInstaller
 {
+    [SerializeField] private RankColorPalette _numberColorPalette;
+
     public override void InstallBindings()
     {
         Container.Bind<IEventBus>().To<EventBus>().AsSingle();
         Container.Bind<IUIMover>().To<UIDoTweenMover>().AsSingle();
+        Container.Bind<RankDeterminer>().FromComponentInHierarchy().AsSingle();
+        Container.Bind<RankColorPalette>().FromInstance(_numberColorPalette).AsSingle();
     }
 }
