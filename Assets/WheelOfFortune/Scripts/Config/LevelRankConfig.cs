f986a3424cf00813ae675741f63beabbc7356819
jackwak <mehmetyanik53@hotmail.com>
2026-05-15 17:11:14 +0300

    Added LevelRankConfig.cs

---
+++ Assets/WheelOfFortune/Scripts/Config/LevelRankConfig.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using WheelOfFortune.Enums;
+namespace WheelOfFortune.Config
+{
+    [CreateAssetMenu(fileName = "LevelRankConfig", menuName = "WheelOfFortune/Config/LevelRankConfig")]
+    public class LevelRankConfig : ScriptableObject
+    {
+        [SerializeField] private LevelRankData[] _rankData;
+
+        public LevelRankData[] RankData => _rankData;
+    }
+    [System.Serializable]
+    public struct LevelRankData
+    {
+        public int RankInterval;
+        public Rank Rank;
+    }
+}
