f0ec7880b5a2c3c54760b697e5cc7e7509998583
jackwak <mehmetyanik53@hotmail.com>
2026-05-15 16:47:44 +0300

    Added LevelDisplayNumberIndicatorBehaviour.cs

---
+++ Assets/WheelOfFortune/Scripts/Gameplay/LevelDisplayManager/LevelDisplayNumberIndicatorBehaviour.cs
@@ -0,0 +1,57 @@
+using UnityEngine.UI;
+using UnityEngine;
+using Zenject;
+using WheelOfFortune.Utils.RankDeterminer;
+using WheelOfFortune.Events;
+using WheelOfFortune.Core.EventBus;
+using WheelOfFortune.Config;
+using WheelOfFortune.Enums;
+
+namespace WheelOfFortune.Gameplay.LevelDisplayManager
+{
+    public class LevelDisplayNumberIndicatorBehaviour : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private Image _backgroundImage;
+        [SerializeField] private RankColorPalette _numberIndicatorRankPalette;
+
+        [Header("Dependencies")]
+        private IEventBus _eventBus;
+        private RankDeterminer _rankDeterminer;
+
+        [Inject]
+        public void Construct(IEventBus eventBus, RankDeterminer rankDeterminer)
+        {
+            _eventBus = eventBus;
+            _rankDeterminer = rankDeterminer;
+        }
+
+        void OnEnable()
+        {
+            _eventBus.Subscribe<LevelChangedEventData>(OnLevelNumberChanged);
+        }
+
+        void OnDisable()
+        {
+            _eventBus.Unsubscribe<LevelChangedEventData>(OnLevelNumberChanged);
+        }
+
+        public void Initialize(int startLevel)
+        {
+            SetBackgroundColor(GetColorByRank(GetRankByLevel(startLevel)));
+        }
+
+        private void OnLevelNumberChanged(LevelChangedEventData eventData)
+        {
+            SetBackgroundColor(GetColorByRank(GetRankByLevel(eventData.NewLevel)));
+        }
+
+        private void SetBackgroundColor(Color color)
+        {
+            _backgroundImage.color = color;
+        }
+
+        private Color GetColorByRank(Rank rank) => _numberIndicatorRankPalette.GetColor(rank);
+        private Rank GetRankByLevel(int level) => _rankDeterminer.DetermineRank(level);
+    }
+}
